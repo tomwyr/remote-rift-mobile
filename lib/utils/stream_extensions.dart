c1c4907438592f82a54b42495f472f8b2ffab609
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-11-17 16:39:25 +0100

    Prevent onDone from being called after error

--- lib/utils/stream_extensions.dart
+++ lib/utils/stream_extensions.dart
@@ -4,11 +4,21 @@ import 'package:flutter/foundation.dart';
 
 extension StreamExtensions<T> on Stream<T> {
   Stream<T> peek({VoidCallback? onFirstOrError, VoidCallback? onDone}) {
+    var errorOccurred = false;
+
     var firstOrErrorCalled = false;
     handleFirstOrError() {
       if (!firstOrErrorCalled) {
         firstOrErrorCalled = true;
         onFirstOrError?.call();
+      }
+    }
+
+    var doneCalled = false;
+    handleDone() {
+      if (!errorOccurred && !doneCalled) {
+        doneCalled = true;
+        onDone?.call();
       }
     }
 
@@ -19,11 +29,12 @@ extension StreamExtensions<T> on Stream<T> {
           sink.add(data);
         },
         handleError: (error, stackTrace, sink) {
+          errorOccurred = true;
           handleFirstOrError();
           sink.addError(error, stackTrace);
         },
         handleDone: (sink) {
-          onDone?.call();
+          handleDone();
           sink.close();
         },
       ),
