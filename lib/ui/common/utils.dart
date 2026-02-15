b6cbe780cdecbc9a5793ffed6bd8c96d29b9945e
Tomasz Wyrowiński <tomwyr@hotmail.com>
2026-02-16 00:02:12 +0100

    Disable vibration when not in foreground (closes RR-84)

--- lib/ui/common/utils.dart
+++ lib/ui/common/utils.dart
@@ -1,6 +1,10 @@
+import 'package:remote_rift_ui/remote_rift_ui.dart';
 import 'package:vibration/vibration.dart';
 
 Future<void> vibrateMillis(int duration) async {
+  if (AppLifecycle.currentState != .resumed) {
+    return;
+  }
   if (await Vibration.hasVibrator()) {
     Vibration.vibrate(duration: duration);
   }
